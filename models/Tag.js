35bf0edeb2ca774335556f829f4a85e006c80e42
Riley Lum <rileylum@adam.com.au>
2021-08-31 16:49:55 +0930

    Add models and relationships

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,20 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, DATE } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
